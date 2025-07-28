e8f7dd38e34aab2baaf636b8d1ade6e2aaea241d
Viktoria Andrushenka <vika.sapriko.9981@gmail.com>
2025-07-29 00:54:30 +0300

    update workflows

--- config/settings.py
+++ config/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv('POSTGRES_DB', 'test_db'),
+        "NAME": os.getenv('POSTGRES_DB', 'medical_db'),
         "USER": os.getenv('POSTGRES_USER'),
         "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
         "HOST": os.getenv('POSTGRES_HOST', 'localhost'),
