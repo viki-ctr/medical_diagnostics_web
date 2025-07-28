bbf35e9c26858fad96abc26a31ae6ba6dfc29e97
Viktoria Andrushenka <vika.sapriko.9981@gmail.com>
2025-07-29 00:42:45 +0300

    update test3

--- config/settings.py
+++ config/settings.py
@@ -100,8 +100,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": os.getenv('POSTGRES_DB', 'test_db'),
-        "USER": os.getenv('POSTGRES_USER', 'postgres'),
-        "PASSWORD": os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        "USER": os.getenv('POSTGRES_USER'),
+        "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
         "HOST": os.getenv('POSTGRES_HOST', 'localhost'),
         "PORT": os.getenv('POSTGRES_PORT' '5432'),
         "OPTIONS": {
